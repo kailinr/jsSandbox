56c54810048998f19f932a9267eeebd696bde516
kailin <kailin@torvadata.com>
2022-02-13 21:14:02 -0800

    Switch Statements

--- app.js
+++ app.js
@@ -1,112 +1,59 @@
-// // if(something){
-// // do something
-// // } else {
-// //   do someting else
-// // }
+const color = 'yellow';
 
-
-const id = '100';
-
-// //EQUAL TO
-// if (id == 1200){
-//   console.log('CORRECT');
-// } else {
-//   console.log("INCORRECT");
-// }
-
-// //NOT EQUAL TO (Same Value)
-// if (id != 101){
-//   console.log('CORRECT');
-// } else {
-//   console.log("INCORRECT");
-// }
-
-// //**EQUAL TO VALUE & TYPE
-// if (id === 100){
-//   console.log('CORRECT');
-// } else {
-//   console.log("INCORRECT");
-// }
-
-// //returns incorrect because its not a string. 
-
-// //NOT EQUAL TO VALUE & TYPE
-// if (id !== 100){
-//   console.log('CORRECT');
-// } else {
-//   console.log("INCORRECT");
-// }
-
-
-// //Adding Template Litral to If Statement to check if ID is assigned (don't verify like this):
-// if (id) {
-//   console.log(`The ID is ${id}`)
-// } else {
-//   console.log('No id');
-// } //Returns The ID is 100, or undefined if id is not defined.
-
-// //**Verify Definition with typeOf & Template Literals like this;
-
-// if (typeof id !== 'undefined') {
-//   console.log(`ID is ${id}`);
-// } else {
-//   console.log("No ID");
-// }
-
-//testing whether the type of variable id is undefined or not. 
-
-
-//GREATER THAN OR LESS THAN (<) or Greater than / equal to (<=)
-
-if (id > 200) {
-  console.log('YES');
-} else {
-  console.log('NO');
+switch (color) {
+  case 'red':
+    console.log("Color is red");
+    break;
+  case 'blue':
+    console.log('Color is Blue');
+    //can add as many cases and breaks as you want
+  default: 
+    console.log('color is not red or blue');
+    break;
 }
 
+//Get Date Switch Statement:
 
-//ELSE IF
+let day; 
 
-const color = 'blue';
+switch (new Date().getDay()){
+  case 0:
+    day = 'Sunday'
+    break;
+  
+    case 1:
+    day = 'Monday'
+    break;
+  
+    case 2:
+      day = 'Tuesday'
+      break;
 
-if (color === 'red'){
-  console.log("color is red");
-} else if (color === 'blue') {
-  console.log("color is blue");
-} else {
-  console.log("color is not red or blue");
+      case 3:
+        day = 'Wednesday'
+        break;
+
+        case 4:
+          day = 'Thursday'
+          break;
+
+          case 5:
+            day = 'Friday'
+            break;
+
+            case 6:
+              day = 'Saturday'
+              break;
+
+              case 7:
+                day = 'Sunday'
+                break;
 }
 
+console.log(`Today is ${day}`);
 
-//LOGICAL OPERATORS 
 
-//&&
-const name = "Steve";
-const age = 75;
 
-if(age > 0 && age < 12){
-  console.log(`${name} is a child`);
-} else if(age >= 13 && age <= 19){
-  console.log(`${name} is a teenage`);
-} else {
-  console.log(`${name} is an adult`);
-}
+const dayTest = new Date().getDate();
 
-//OR ||
-if (age < 16 || age > 65){
-  console.log(`${name} cannot run in race`);
-} else {
-  console.log(`${name} is registered for the race`);
-}
-
-//TERNARY OPERATOR
-id === '100' ? console.log("Yes") : console.log("No");
-//Better
-console.log(id === '100' ? "Yes" : "No");
-
-//WITHOUT BRACES
-
-if (id === 100) 
-  console.log('Yes');
-else
-  console.log('No');
+console.log(dayTest);
