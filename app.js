b00d944d374935dfda2bc58b8969e5cea44821d3
kailin <kailin@torvadata.com>
2022-02-14 13:00:53 -0800

    Window

--- app.js
+++ app.js
@@ -1,73 +1,20 @@
+//WINDOW METHODS / OBJECTS / PROPERTIES
 
-let i = 0;
+// window.console.log(123);
+//same as log
 
-while (i < 10) {
-  console.log(`num ${i}`);
-  i++
-}
+//ALERT
+// window.alert('Hello World');
+//same as alert
 
+//PROMPT - asks and then alerts
+// const input = prompt();
+// alert(input);
 
-
-//DO WHILE LOOP
-
-let b = 0;
-
-do {
-  console.log('testing' + b);
-  b++
-}
-while (b < 3);
-
-
-//Loop Through Arrays
-
-const cars = ['ford', 'chevy', 'honda', 'toyota'];
-
-for (let i = 0; i < cars.length; i++) {
-  console.log(cars[i]);
-}
-
-//For Each Loop w/ Array
-cars.forEach(function(car){
-  console.log(car);
-});
-
-
-//For Each Loop w/ Array - w. Value & Index Params
-cars.forEach(function(car, index){
-  console.log(`${index} : ${car}`);
-});
-
-//For Each Loop w/ Array - w. Value & Index & Array Params
-cars.forEach(function(car, index, array){
-  console.log(`${index} : ${car}`);
-  console.log(array);
-});
-
-
-
-//MAP
-//Loop through an array of objects
-const users = [
-  {id:1, name:'john'},
-  {id:2, name:'Sue'},
-  {id:3, name:'Sam'},
-];
-
-const ids = users.map(function(user){
-  return user.id;
-});
-
-console.log(ids);
-
-
-//FOR IN LOOP
-const newUser = {
-  first: 'John',
-  last: 'Samson',
-  age: 50
+//CONFIRM - used to confirm aciton like a delete something
+//returns yes in console if you click yes, and no if you click no. 
+if (confirm('are you sure?')){
+  console.log('Yes');
+} else {
+  console.log('No');
 };
-
-for(let x in newUser){
-  console.log(`${x} : ${newUser[x]}`);
-}
